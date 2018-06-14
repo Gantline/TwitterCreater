4744570d719fb117cbd4fa2593f2a8c263ed21b6
Gantline <37279443+Gantline@users.noreply.github.com>
2018-06-14 11:51:34 -0400

    Fixed NewsGatherer Url Call (#3)
    * Fixed NewsGatherer Url Call
    
    Changed news q to tall ships regatta
    temp fix to get request
    removed news gatherer sort by - defaulting to sort by best
    only posting 1 story per run
    
    * encode log to utf8

--- NewsGatherer.py
+++ NewsGatherer.py
@@ -7,13 +7,15 @@ import datetime
 def get_news_stories(): #TODO change url query to "tall%20ships"
     url = 'https://newsapi.org/v2/everything'
     data = {
-        'q' : 'tall%20ships',
-        'from' :  str(datetime.date.today() - datetime.timedelta(days=1)), #TODO make today - 2
+        'q' : 'tall%20ships%20regatta',
+        'from' :  str(datetime.date.today() - datetime.timedelta(days=1)),
         'sortBy' : 'publishedAt',
         'apiKey' : newsgathererKey,
-        'to': str(datetime.date.today() - datetime.timedelta(days=1)),  # TODO make today - 2
+        'to': str(datetime.date.today() - datetime.timedelta(days=1)),
     }
-    r = requests.get(url, data)
+    print(url+'?q='+data['q']+'&from='+data['from']+'&to='+data['to']+'&apiKey='+data['apiKey'])
+    #r = requests.get(url, data) ##< returning {u'status': u'ok', u'articles': [], u'totalResults': 0}
+    r = requests.get(url+'?q='+data['q']+'&from='+data['from']+'&to='+data['to']+'&apiKey='+data['apiKey']) #TODO make this more like get(url, data)  ALSO, i've removed sortBy to default to sortBy=best
     return r.json()
 
 def parse_news_stories(response, story): #Takes json responce from get_news_stories() and a int of which story you would like
